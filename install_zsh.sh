e29c1c2328768eb28cf40c220b36b4ecd943d17c
sonulen <sonulen@gmail.com>
2018-10-04 16:09:09 +0300

    fix: updateinstall zsh

--- install_zsh.sh
+++ install_zsh.sh
@@ -4,6 +4,7 @@ sudo apt-get install zsh
 chsh -s $(which zsh)
 zsh --version
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 cd ~;
 git clone git://github.com/wting/autojump.git
 cd autojump

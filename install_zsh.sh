bbc11ddf6c0fa5d6b94e03cc02bc0885804e9e60
sonulen <sonulen@gmail.com>
2018-11-16 12:15:04 +0300

    fix: fix zsh installer

--- install_zsh.sh
+++ install_zsh.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
-
+cd ~;
 sudo apt-get install zsh
 sudo apt-get install autojump
 chsh -s $(which zsh)
 zsh --version
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-cd ~;
-git clone git://github.com/wting/autojump.git
-cd autojump
-./install.py
 cp .zshrc ~/.zshrc
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
@@ -16,5 +12,9 @@ wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbol
 mkdir ~/.local/share/fonts/
 mv PowerlineSymbols.otf ~/.local/share/fonts/
 fc-cache -vf ~/.local/share/fonts/
+mkdir ~/.config/fontconfig/
 mkdir ~/.config/fontconfig/conf.d/
 mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
+git clone git://github.com/wting/autojump.git
+cd autojump
+./install.py

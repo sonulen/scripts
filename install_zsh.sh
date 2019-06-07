92325b61c73656cb61f636697ccf760b748c6f4e
sonulen <sonulen@gmail.com>
2019-06-07 16:20:50 +0300

    update: new version install zsh

--- install_zsh.sh
+++ install_zsh.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 cd ~;
 sudo apt-get install zsh
-sudo apt-get install autojump
-chsh -s $(which zsh)
-zsh --version
+
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-cp .zshrc ~/.zshrc
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
 wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
@@ -15,6 +12,42 @@ fc-cache -vf ~/.local/share/fonts/
 mkdir ~/.config/fontconfig/
 mkdir ~/.config/fontconfig/conf.d/
 mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
+
 git clone git://github.com/wting/autojump.git
 cd autojump
 ./install.py
+
+# Highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+rm -rf zsh-syntax-highlighting
+
+# Autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
+cat<< EOF > ~/.zshrc
+# sonulen edition
+
+POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(context dir_writable dir vcs)
+POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=()
+
+# Path to your oh-my-zsh installation.
+export ZSH=$HOME/.oh-my-zsh
+
+ZSH_THEME="powerlevel9k/powerlevel9k"
+
+plugins=(
+  git
+  zsh-autosuggestions
+)
+
+source $ZSH/oh-my-zsh.sh
+
+[[ -s /home/sonulen/.autojump/etc/profile.d/autojump.sh ]] && source /home/sonulen/.autojump/etc/profile.d/autojump.sh
+
+autoload -U compinit && compinit -u
+source /home/sonulen/wrk/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+EOF
+
+source .zshrc
+chsh -s $(which zsh)
